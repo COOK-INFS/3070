5bcb3999ae9a370295f99ca6c88b339fb9c23e87
infs3080 <44375801+infs3080@users.noreply.github.com>
2022-02-17 14:46:25 -0700

    Do While and While examples

---
+++ src/whileLoopExample.java
@@ -0,0 +1,17 @@
+
+public class whileLoopExample {
+
+	public static void main(String[] args) {
+
+		int number = 1;
+		
+		while (number <=5) {
+			System.out.println("Not Yet");
+			number ++;
+		}
+
+		System.out.println("All done counting!");
+		
+	}
+
+}

ba1628206d8e6f5666c59bd7f4ac4ec81a59ecb5
infs3080 <44375801+infs3080@users.noreply.github.com>
2022-03-31 14:17:50 -0600

    Method in a Class example

--- src/classExample.java
+++ src/classExample.java
@@ -1,7 +1,7 @@
 
 public class classExample {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		
 		vehicle fordF350 = new vehicle(); // Create the object of the class vehicle
 		
@@ -9,6 +9,7 @@ public class classExample {
 		fordF350.economy = 18;
 		fordF350.speed = 100;
 		fordF350.passengers = 5;
+		fordF350.fuelCapacity = 35;
 				
 		
 		
@@ -18,10 +19,17 @@ public class classExample {
 		jeepWrangler.economy = 24;
 		jeepWrangler.speed = 80;
 		jeepWrangler.passengers = 5;
+		jeepWrangler.fuelCapacity = 22;
 		
 		
 		System.out.println("The F350 can hold " + fordF350.passengers + " passengers.");
 		System.out.println("The Jeep Wrangler gets " + jeepWrangler.economy + " mpg.");
+		
+		System.out.println("F350: ");
+		fordF350.estimatedRange();
+		
+		System.out.println("Jeep: ");
+		jeepWrangler.estimatedRange();
 
 		
 	}
@@ -32,5 +40,16 @@ class vehicle{
 	String type;
 	int passengers;
 	int speed;
-	int economy; 
+	int economy;
+	int fuelCapacity;
+	
+	void estimatedRange() {
+		double range;
+		
+		range = fuelCapacity * economy;
+		
+		System.out.println("The estimated range is: " + range + " miles.");
+		
+		
+	}
 }
